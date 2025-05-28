220aece17b4e6fcd5b026fe1686087f411cd6d35
AbhinavRaman <abhinavraman102@gmail.com>
2025-05-28 12:14:24 +0530

    made a media directory and added a video in hero.jsx

--- src/assets/Hero.jsx
+++ src/assets/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from './Button'
+import MainVideo from '../Media/LetsBlog_MainVideo.mp4'
 
 const Hero = () => {
   return (
@@ -8,6 +9,7 @@ const Hero = () => {
             <h1 className='text-4xl mb-4'>Blog with the best</h1>
             <p className='mb-4'>More bloggers and independent creators choose WordPress than any other blogging tool. Tap into intuitive, flexible tools that put writers, bloggers, and creators first.</p>
             <Button />
+            <video src={MainVideo} typeof='video/mp4' autoPlay loop muted className='rounded m-5 border-blue-400 border-8'></video>
         </div>
     </div>
   )
