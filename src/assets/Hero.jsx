bf0b6f3a07f16555bfc57d080db47bb241302709
AbhinavRaman <abhinavraman102@gmail.com>
2025-06-03 16:20:09 +0530

    added Accrodian file

--- src/assets/Hero.jsx
+++ src/assets/Hero.jsx
@@ -9,13 +9,13 @@ const Hero = () => {
       <div className='w-full h-screen bg-white justify-center'>
         <div className='w-full h-3/4 bg-[#151414] text-white flex flex-col p-4'>
             <h1 className='text-4xl mb-4'>Blog with the best</h1>
-            <p className='mb-4'>More bloggers and independent creators choose WordPress than any other blogging tool. Tap into intuitive, flexible tools that put writers, bloggers, and creators first.</p>
+            <p className='mb-4'>More bloggers and independent creators choose LetsBlog than any other blogging tool. Tap into intuitive, flexible tools that put writers, bloggers, and creators first.</p>
             <Button />
             <video src={MainVideo} typeof='video/mp4' autoPlay loop muted className='rounded-2xl m-5 border-blue-400 border-8'></video>
         </div>
         <div className='w-full flex flex-col p-4'>
           <h1 className='text-3xl mb-4'>Simple, meet flexible.</h1>
-          <p className='mb-4'>Whatever you’re publishing. Whoever your audience is. WordPress.com makes it simple to get started. And easy to expand your site as your audience grows.</p>
+          <p className='mb-4'>Whatever you’re publishing. Whoever your audience is. LetsBlog.com makes it simple to get started. And easy to expand your site as your audience grows.</p>
           <Button />
         </div>
         <div className='w-full flex items-center justify-center'>
