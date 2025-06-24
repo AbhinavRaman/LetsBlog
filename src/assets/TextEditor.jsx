a4b2be0b50957fbec3066bfd1ff0142bbb9dd401
AbhinavRaman <abhinavraman102@gmail.com>
2025-06-24 13:37:26 +0530

    fixed text area in texteditor.jsx

--- src/assets/TextEditor.jsx
+++ src/assets/TextEditor.jsx
@@ -8,7 +8,7 @@ const TextEditor = () => {
           <label htmlFor="title" className='text-6xl m-4'>Title</label><br />
           <input type="text" className='border rounded p-2 m-4 w-1/2'/> <br />
           <label htmlFor="body" className='text-3xl m-4'>Body</label> <br />
-          <textarea name="body" id="body" className='m-4 border rounded w-1/2'></textarea>
+          <textarea name="body" id="body" className='m-4 border rounded w-1/2 h-[200px] p-2'></textarea>
         </form>
       </div>
     </div>
